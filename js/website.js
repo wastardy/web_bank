ee753b6ba59e5b1b157cf610b75a51cc199dcf71
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-20 14:58:43 +0200

    add initial files for work with bank website part

---
+++ js/website.js
@@ -0,0 +1,35 @@
+'use strict';
+
+//#region Query Selectors
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+//#endregion
+
+//#region Methods
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+//#endregion
+
+//#region Event Listeners
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' 
+        && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
+//#endregion
